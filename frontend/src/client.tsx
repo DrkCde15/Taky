aeae074545ed57c4ab44790a56297a4512ed8e57
DrkCde15 <jcesarsantana215@gmail.com>
2026-06-29 12:20:07 -0300

    index/dist

---
+++ frontend/src/client.tsx
@@ -0,0 +1,5 @@
+import { StartClient } from "@tanstack/react-start-client";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+createRoot(document.getElementById("root")!).render(<StartClient />);
