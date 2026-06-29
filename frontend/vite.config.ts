4c3284822e7094114d9561dd3f006660967f825d
DrkCde15 <jcesarsantana215@gmail.com>
2026-06-29 14:41:54 -0300

    new plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,8 +6,10 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   base: process.env.VITE_BASE_PATH || '/',
 });
